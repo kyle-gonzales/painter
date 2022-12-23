17f44882cd3e8629d4fbb820e4d2deb1626e0c35
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2022-12-23 18:35:28 +0800

    learned how to use permissions

--- app/src/main/java/com/example/painter/MainActivity.kt
+++ app/src/main/java/com/example/painter/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.painter
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.app.Dialog
+import android.app.Instrumentation.ActivityResult
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +14,8 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import yuku.ambilwarna.AmbilWarnaDialog
@@ -21,6 +27,48 @@ class MainActivity : AppCompatActivity() {
     private var ibCurrentBrushColor : ImageButton? = null
 
     var tvCustomColorBrush : TextView? = null
+    /*permission result launcher for one permission*/
+    private val cameraResultLauncher : ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+            {
+                /*renaming 'it' to isGranted */
+                isGranted -> if(isGranted){
+                    Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "permission DENIED", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    /*permission result launcher for MULTIPLE permissions*/
+
+    private val cameraAndLocationResultLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            /*renaming 'it : Map<String, Boolean>!' to permissions */
+                permissions -> permissions.entries.forEach {
+                    /* Entry<String!, Boolean!> in map*/
+                    val permissionName = it.key
+                    val isGranted = it.value
+                    if (isGranted) {
+                        /*check which permission is granted*/
+                        if (permissionName == Manifest.permission.CAMERA) {
+                            Toast.makeText(this, "permission granted for CAMERA", Toast.LENGTH_SHORT).show()
+                        } else if (permissionName == Manifest.permission.ACCESS_COARSE_LOCATION){
+                            Toast.makeText(this, "permission granted for approximate LOCATION", Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(this, "permission granted for precise LOCATION", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        if (permissionName == Manifest.permission.CAMERA) {
+                            Toast.makeText(this, "permission DENIED for CAMERA", Toast.LENGTH_SHORT).show()
+                        } else if (permissionName == Manifest.permission.ACCESS_COARSE_LOCATION){
+                            Toast.makeText(this, "permission DENIED for approximate LOCATION", Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(this, "permission DENIED for precise LOCATION", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                 }
+
+        }
 
 
     @SuppressLint("MissingInflatedId")
@@ -34,6 +82,19 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawingView)
         drawingView!!.setBrushSize(10f)
+
+        val btnCameraPermission : ImageButton = findViewById(R.id.ibSetBackground)
+        btnCameraPermission.setOnClickListener {
+            // if we have already asked for permission, yet the user did not grant access, display a dialog
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {/*check for the android version*/ /*Android M => Android 4.1*/
+                showRationalDialog("Painter Requires Camera", "Camera cannot be used because camera access is denied")
+            } else { // request for permission first
+                cameraAndLocationResultLauncher.launch(
+                    arrayOf(Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION))
+            }
+        }
 
 
         val ibBrushSize = findViewById<ImageButton>(R.id.ibBrushSize)
@@ -71,6 +132,16 @@ class MainActivity : AppCompatActivity() {
             openColorPickerDialogue()
             brushColorClicked(tvCustomColorBrush)
         }
+    }
+
+    private fun showRationalDialog(title: String, message: String) {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
+        builder.create()
+        builder.show()
     }
 
     private fun openColorPickerDialogue() {
